7d9d55512064955f62cef486d434bfb3cc9c26f1
Vennavaram <S538349@nwmissouri.edu>
2020-02-17 14:40:16 -0600

    app

---
+++ calc.js
@@ -0,0 +1,9 @@
+function Calculate(){
+  let totalvalue = parseFloat(document.getElementById("totalvalue").value);
+  console.log(typeof totalvalue);
+  let ratevalue = parseFloat(document.getElementById("ratevalue").value);
+  document.getElementById("tipvalue").innerHTML = "$" + totalvalue*ratevalue/100;
+  document.getElementById("taxvalue").innerHTML = "$" + totalvalue*5.50/100;
+  document.getElementById("grandvalue").innerHTML = "$" + (totalvalue  + (totalvalue*ratevalue/100) + (totalvalue*5.50/100));
+  return false;
+}
