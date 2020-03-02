8f460b6f4729cef15587b87782a35ecfff769205
vishal Reddy <vishalreddy0507@gmail.com>
2020-03-02 10:17:36 -0600

    added area

--- calc.js
+++ calc.js
@@ -1,9 +1,8 @@
 function Calculate(){
-  let totalvalue = parseFloat(document.getElementById("totalvalue").value);
-  console.log(typeof totalvalue);
-  let ratevalue = parseFloat(document.getElementById("ratevalue").value);
-  document.getElementById("tipvalue").innerHTML = totalvalue*ratevalue/100;
-  document.getElementById("taxvalue").innerHTML = totalvalue*5.50/100;
-  document.getElementById("grandvalue").innerHTML = (totalvalue  + (totalvalue*ratevalue/100) + (totalvalue*5.50/100));
+  let Area = parseFloat(document.getElementById("Area").value);
+  console.log(typeof Area);
+  let radius = parseFloat(document.getElementById("radius").value);
+  document.getElementById("Area").innerHTML = 3.14*radius*radius;
+ 
   return false;
 }
