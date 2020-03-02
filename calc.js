fd7475d9171f76181ffa1751c9801139b8c0ac5d
vishal Reddy <vishalreddy0507@gmail.com>
2020-03-02 10:22:01 -0600

    radius value

--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
 function Calculate(){
   let Area = parseFloat(document.getElementById("Area").value);
   console.log(typeof Area);
-  let radius = parseFloat(document.getElementById("radius").value);
+  let radius = parseFloat(document.getElementById("radiusvalue").value);
   document.getElementById("Area").innerHTML = 3.14*radius*radius;
  
   return false;
