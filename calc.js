4ef106889b24419cac278208f8292afae663a7f9
vishal Reddy <vishalreddy0507@gmail.com>
2020-03-02 10:27:47 -0600

    added radius

--- calc.js
+++ calc.js
@@ -1,8 +1,8 @@
 function Calculate(){
-  let Area = parseFloat(document.getElementById("Area").value);
-  console.log(typeof Area);
-  let radius = parseFloat(document.getElementById("radiusvalue").value);
-  document.getElementById("Area").innerHTML = 3.14*radius*radius;
+  let radiusvalue = parseFloat(document.getElementById("radiusvalue").value);
+  console.log(typeof totalvalue);
+ 
+  document.getElementById("areavalue").innerHTML = 3.14*radius*radius;
  
   return false;
 }
